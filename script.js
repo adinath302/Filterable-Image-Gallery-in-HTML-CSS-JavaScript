583d2a54fb0693d425823a0960adf070d049e774
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-01-16 18:18:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const filterbuttons = document.querySelectorAll(".filter_buttons button");
+const filterablecards = document.querySelectorAll(".filterable_cards .card");
+
+const filtercards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterablecards.forEach(card => {
+        card.classList.add("hide");
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card.classList.remove("hide")
+        }
+        // console.log(e.target);
+    });
+};
+
+filterbuttons.forEach(button => button.addEventListener("click", filtercards));
